e237099af5de5a3919fd6cf31c607af94056403b
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-03 10:17:10 -0600

    ingredient class and test created

---
+++ lib/ingredient.rb
@@ -0,0 +1,10 @@
+class Ingredient
+  attr_reader :name, :unit, :calories
+
+  def initialize(ingredient_info)
+    @name = ingredient_info[:name]
+    @unit = ingredient_info[:unit]
+    @calories = ingredient_info[:calories]
+  end 
+
+end
